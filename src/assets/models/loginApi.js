cee9efbbed310438bf452f0960bf6f3fa1c72693
wangbo <wb549791>
2019-04-17 11:47:35 +0800

    提交登录

--- src/assets/models/loginApi.js
+++ src/assets/models/loginApi.js
@@ -5,9 +5,12 @@ const prefixUA=Urls.UA;
 const prefix=Urls.supplyChainUrl;
 const prefixEA=Urls.EAUrl;
 export default {
+
+
+
   //UA登陆，拿到token
   getUALogin(params={}){
-    return axios.post(prefixUA+'user/login',params);
+    return axios.post(prefixUA+'/login/pcLogin',params);
   },
   //登陆接口
   getLoginInfo(params={}){
