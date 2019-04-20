222f68ed706b31fa1a23d3be50e498df4ec62783
wangbo <wb549791>
2019-04-20 10:06:30 +0800

    删除多余

---
+++ src/pages/PermissionManage/TreeTable.vue
@@ -0,0 +1,268 @@
+<template>
+  <el-table :data="formatData" :row-style="showRow" v-bind="$attrs"  >
+    <el-table-column v-if="columns.length===0" width="150"  >
+      <template slot-scope="scope">
+        <span v-for="space in scope.row._level" :key="space" class="ms-tree-space"/>
+        <span v-if="iconShow(0,scope.row)" class="tree-ctrl" @click="toggleExpanded(scope.$index)">
+          <i v-if="!scope.row._expanded" class="el-icon-plus"/>
+          <i v-else class="el-icon-minus"/>
+        </span>
+        {{ scope.$index }}
+      </template>
+    </el-table-column>
+    <el-table-column v-for="(column, index) in columns"  v-else :key="column.value" :label="column.text" :width="column.width" >
+
+      <template slot-scope="scope" >
+          <span v-for="space in scope.row._level" v-if="index === 0" :key="space" class="ms-tree-space"/>
+          <span v-if="iconShow(index,scope.row)" class="tree-ctrl" @click="toggleExpanded(scope.$index)">
+          <i v-if="!scope.row._expanded" class="el-icon-plus"/>
+          <i v-else class="el-icon-minus"/>
+        </span>
+         <span >
+
+           <span v-if="column.text == '状态'">{{ scope.row[column.value] == 1 ? "启用" :"停用" }}</span>
+            <span v-if="column.text == '角色名称'">{{ scope.row[column.value] }}</span>
+           <span v-if="column.text == '创建时间'"> {{ scope.row[column.value] }}</span>
+           <span v-if="column.text == '创建人'">{{ scope.row[column.value] }}</span>
+           <span v-if="column.text == '唯一编号'">{{ scope.row[column.value] }}</span>
+         </span>
+      </template>
+    </el-table-column>
+
+    <el-table-column align="center" label="操作" v-if="treeType === 'normal'" width="250">
+      <template slot-scope="props">
+        <el-button type="text" size="small" @click="upRole(props.row)" >编辑</el-button>
+        <el-button type="text" size="small" @click="infoRole(props.row)" >详情</el-button>
+        <el-button type="text" size="small" @click="ischangeRole(props.row)" >{{props.row.state == 1 ? '停用':'启用'}}</el-button>
+        <el-button type="text" size="small" @click="delRole(props.row)" >刪除</el-button>
+      </template>
+    </el-table-column>
+
+    <slot/>
+
+  </el-table>
+</template>
+
+<script>
+  //引入 vue 后台发请求api接口
+  import treeToArray from "./eval";
+  export default {
+    name: "TreeTable",
+    props: {
+      // 该属性是确认父组件传过来的数据是否已经是树形结构了，如果是，则不需要进行树形格式化
+      treeStructure: {
+        type: Boolean,
+        default: function () {
+          return false
+        }
+      },
+      /* eslint-disable */
+      data: {
+        type: [Array, Object],
+        required: true
+      },
+      columns: {
+        type: Array,
+        default: () => []
+      },
+      evalFunc: Function,
+      evalArgs: Array,
+      expandAll: {
+        type: Boolean,
+        default: false
+      },
+      // 这是数据源
+      dataSource: {
+        type: Array,
+        default: function () {
+          return []
+        }
+      },
+// 这个作用是根据自己需求来的，比如在操作中涉及相关按钮编辑，删除等，需要向服务端发送请求，则可以把url传过来
+      requestUrl: {
+        type: String,
+        default: function () {
+          return ''
+        }
+      },
+// 这个是是否展示操作列
+      treeType: {
+        type: String,
+        default: function () {
+          return 'normal'
+        }
+      },
+// 是否默认展开所有树
+      defaultExpandAll: {
+        type: Boolean,
+        default: function () {
+          return false
+        }
+      },
+
+    },
+    computed: {
+      // 格式化数据源
+      formatData: function() {
+        let tmp;
+        if (!Array.isArray(this.data)) {
+          tmp = [this.data];
+        } else {
+          tmp = this.data;
+        }
+        const func = this.evalFunc || treeToArray;
+        const args = this.evalArgs
+          ? Array.concat([tmp, this.expandAll], this.evalArgs)
+          : [tmp, this.expandAll];
+        return func.apply(null, args);
+      }
+    },
+    filters: {
+      btnType (value) {
+        if(value === 'M') {
+          return'菜单'
+        } else if (value === 'B'){
+          return '按钮'
+        } else {
+          return value
+        }
+      }
+    },
+    components: {
+    },
+    methods: {
+      showRow: function(row) {
+        const show = row.row.parent
+          ? row.row.parent._expanded && row.row.parent._show
+          : true;
+        row.row._show = show;
+        return show
+          ? "animation:treeTableShow 1s;-webkit-animation:treeTableShow 1s;"
+          : "display:none;";
+      },
+      // 切换下级是否展开
+      toggleExpanded: function(trIndex) {
+        const record = this.formatData[trIndex];
+        record._expanded = !record._expanded;
+      },
+      // 图标显示
+      iconShow(index, record) {
+        return index === 0 && record.children && record.children.length > 0;
+      },
+
+      //改变状态
+      ischangeRole(val){
+        this.$confirm('确定要改变当前数据状态吗, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '要执行的数据：'+val.id
+          });
+        }).catch(() => {
+          console.log("取消");
+        });
+      },
+      //删除
+      delRole(val){
+        try {
+          if(val.children.length > 0){
+            this.$alert('请先删除子级节点', '存在子级节点', {
+              confirmButtonText: '确定',
+              callback: action => {
+              }
+            });
+          }else{
+            this.$confirm('确定要刪除当前数据状态吗, 是否继续?', '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              this.$message({
+                type: 'success',
+                message:  '要执行的数据：'+val.id
+              });
+            }).catch(() => {
+              console.log("取消");
+            });
+          }
+        }catch (e) {
+          this.$message("数据出错!");
+        }
+      },
+      //详情
+      infoRole(val){
+        this.$message({
+          type: 'success',
+          message: '要执行的数据：'+val.id
+        });
+      },
+      //修改
+      upRole(val){
+        this.$message({
+          type: 'success',
+          message: '要执行的数据：'+val.id
+        });
+      },
+    },
+    data(){
+      return {
+        hackReset:false,
+      }
+    },
+    //页面载入  数据处理
+    mounted:function(){
+    }
+  };
+
+
+</script>
+<style rel="stylesheet/css">
+  @keyframes treeTableShow {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  @-webkit-keyframes treeTableShow {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+</style>
+
+<style scoped>
+  .ms-tree-space {
+    position: relative;
+    top: 1px;
+    display: inline-block;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1;
+    width: 18px;
+    height: 14px;
+  }
+  .ms-tree-space::before {
+    content: "";
+  }
+  .processContainer {
+    width: 100%;
+    height: 100%;
+  }
+  table td {
+    line-height: 26px;
+  }
+  .tree-ctrl {
+    position: relative;
+    cursor: pointer;
+    color: #2196f3;
+    margin-left: -18px;
+  }
+</style>
