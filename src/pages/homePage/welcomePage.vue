ebf1e4a09ee7d279114bae66ef8306c14929c329
wangbo <wb549791>
2019-04-25 11:53:11 +0800

    增加默认登录跳转页面

---
+++ src/pages/homePage/welcomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="welcomePage" style="margin:0 auto;width:300px;height:100px">
+    <H1> {{msg}}</H1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "welcomePage",
+        data(){
+          return {
+            msg:'易经销后台管理系统！'
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
