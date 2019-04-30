a2479ac8cc3025c4ed46565c7cf1ae747c0fffc8
wangbo <wb549791>
2019-04-30 11:57:18 +0800

    备份4.29之前的逻辑页面 更改4.30之后业务页面

--- src/router/index.js
+++ src/router/index.js
@@ -68,6 +68,9 @@ import suserUpdate from '../pages/UserManage/suserUpdate'
 import suser_roleAuthorization from '../pages/UserManage/suser_roleAuthorization'
 import suser_roleAuthorizationcp from '../pages/UserManage/suser_roleAuthorizationcp'
 import suserInfo from '../pages/UserManage/suserInfo'
+import suserAddcp from '../pages/UserManage/suserAddcp'
+import suserUpdatecp from '../pages/UserManage/suserUpdatecp'
+import toexaminebossManage from '../pages/UserManage/toexaminebossManage'
 
 //功能管理
 import  functionManage from '../pages/PermissionManage/functionManage'
@@ -539,6 +542,21 @@ export default new Router({
       path: '/welcomePage',
       name: 'welcomePage',
       component: welcomePage
+    },
+    {
+      path: '/suserAddcp',
+      name: 'suserAddcp',
+      component: suserAddcp
+    },
+    {
+      path: '/suserUpdatecp',
+      name: 'suserUpdatecp',
+      component: suserUpdatecp
+    },
+    {
+      path: '/toexaminebossManage',
+      name: 'toexaminebossManage',
+      component: toexaminebossManage
     }
   ]
 })
