0255d1898f507cf026269be80df1c51251ee8bf9
wangbo <wb549791>
2019-05-31 10:38:55 +0800

    日志管理1111

--- src/pages/LogManage/logManage.vue
+++ src/pages/LogManage/logManage.vue
@@ -81,13 +81,13 @@
   import https_f from 'http/logManageApi'
   import {formatDate} from './dateHandle'
 
-  // 引入基本模板
+/*  // 引入基本模板
   let echarts = require('echarts/lib/echarts')
   // 引入柱状图组件
   require('echarts/lib/chart/bar')
   // 引入提示框和title组件
   require('echarts/lib/component/tooltip')
-  require('echarts/lib/component/title')
+  require('echarts/lib/component/title')*/
 
   export default {
     name: "logManage",
