809a1ceed7b52c77b41f3af18d4404003a38f177
wangbo <wb549791>
2019-06-05 15:19:41 +0800

    调整页面1

--- src/pages/dealerManage/dealerManage.vue
+++ src/pages/dealerManage/dealerManage.vue
@@ -15,7 +15,7 @@
         <el-input v-model="dealerInfo.shopName" placeholder="请输入店铺名称" clearable></el-input>
       </el-form-item>
 
-      <el-form-item label="店铺认证状态" prop="shopType">
+      <el-form-item label="认证状态" prop="shopType">
         <el-select v-model="dealerInfo.shopType" placeholder="请选择用户类型">
           <el-option label="请选择" :value="6"></el-option>
           <el-option label="已认证" :value="1"></el-option>
@@ -49,36 +49,31 @@
           <span>{{indexMethods(scope.$index)}}</span>
         </template>
       </el-table-column>
-      <el-table-column prop="shopName" label="所属名称" width="250">
+      <el-table-column prop="shopName" label="当前经销商所属店铺" width="250">
       </el-table-column>
-      <el-table-column prop="phone" label="手机号" width="150">
-      </el-table-column>
-      <el-table-column prop="shopType" label="所属店铺认证状态" width="120">
-        <template slot-scope="scope">
-          <span>{{scope.row.shopType===1?'已认证':scope.row.shopType==0?'认证中':'未认证' }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="type" label="经销商类型" width="120">
-        <template slot-scope="scope">
-          <span>{{scope.row.type===1?'店老板':scope.row.type==2?'员工':'异常'}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="permissionState" label="经销商级别" width="120">
-        <template slot-scope="scope">
-          <span>{{scope.row.permissionState===1?'管理员':scope.row.type = 1 ?  '老板' : '普工'}}</span>
-        </template>
+      <el-table-column prop="phone" label="当前经销商手机号" width="150">
       </el-table-column>
 
-      <el-table-column prop="createTime" label="创建时间" width="250">
+      <el-table-column prop="wxNickName" label="经销商微信昵称" width="170">
+      </el-table-column>
+
+      <el-table-column  label="所属店铺认证状态" width="160">
+        <template slot-scope="scope">
+          <span>{{scope.row.shopType==1?'已认证':scope.row.shopType==0?'认证中':'未认证' }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="dealertypename"  label="经销商类型" width="160">
+      </el-table-column>
+      <el-table-column  prop="dealerperm" label="经销商级别" width="160">
+      </el-table-column>
+
+      <el-table-column prop="createDate" label="创建时间" width="250">
       </el-table-column>
       <el-table-column fixed="right" label="操作"  >
         <template slot-scope="scope">
-          <!-- 经销商-->
-          <div v-if="scope.row.userType == 0">
-            <el-button type="text" size="small" @click="updateuser(scope.row)">编辑</el-button>
+           <!-- <el-button type="text" size="small" @click="updateuser(scope.row)">编辑</el-button>
             <el-button type="text" size="small" @click="closeorstrat(scope.row)">{{scope.row.state == 1 ? '停用' : '启用'}}</el-button>
-            <el-button type="text" size="small" @click="deleteuser(scope.row)">刪除</el-button>
-          </div>
+            <el-button type="text" size="small" @click="deleteuser(scope.row)">刪除</el-button>-->
         </template>
       </el-table-column>
     </el-table>
@@ -120,15 +115,6 @@
           phone:'',
           auditState:6,
         },
-        applyInfo:{
-          // shopName:'',
-          phone:'',
-          address:'',
-          name:'',
-          createDate:'',
-          logoIamgeUrls:[],
-          userId:'',
-        },
         chargeDialog: false,
         totalCount: 0,
         dealerList: [],
@@ -139,142 +125,9 @@
       AdminCitySelector
     },
     methods: {
-      //店铺老板申请 审核
-      toexamine_boss(){
-        //toexaminebossManage
-        this.$router.push({name:'toexaminebossManage'});
-      },
-      qxshck(){
-        this.chargeDialog = false;
-        this.fdimg = '';
-        this.isshow = false;
-      },
-      isFD(inx){
-        let imgs = this.applyInfo.logoIamgeUrls[inx];
-        this.fdimg = imgs;
-        this.isshow = true;
-      },
-      close_img(){
-        this.fdimg = '';
-        this.isshow = false;
-      },
       formatDate_(time){
         let date = new Date(time);
         return formatDate(date,'yyyy-MM-dd hh:mm:ss');
-      },
-      //审核
-      to_examine(row){
-        let ids = {
-          id:row.id,
-        }
-
-        https_f.findApplyDealer(ids).then(data => {
-          this.loading = false
-          console.log(JSON.stringify(data));
-          // data.logoIamgeUrls.push('http://yjp-dev-articlesharing.ufile.ucloud.cn/easysale/2019/04/d7d3cd15abb548588a30949396491085.png');
-          // data.logoIamgeUrls.push('http://yjp-dev-articlesharing.ufile.ucloud.cn/easysale/2019/04/5918cf19c9c348babfd3debcd9ee528a.png');
-          this.applyInfo  = data;
-          this.chargeDialog = true;
-        }).catch(e => {
-          this.$message('数据异常')
-          this.loading = false
-        })
-      },
-      // 确认审核
-      ckto_examine(){
-        if(this.applyInfo == null){
-          this.fdimg = '';
-          this.isshow = false;
-          this.chargeDialog = false;
-          this.$message("数据异常审核失败!");
-        }else{
-          this.$confirm('确定要审核当前用户吗, 是否继续?', '提示', {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
-            type: 'warning'
-          }).then(() => {
-            this.fdimg = '';
-            this.isshow = false;
-            this.chargeDialog = false;
-            let par = {
-              id:this.applyInfo.userId
-            }
-            https_f.userApplyDer(par).then(data => {
-              this.loading = false
-              this.$message({
-                type: 'success',
-                message: '审核成功'
-              });
-              //变更成经销商   这个只能改变当条数据  经销商的值  改变不了  状态 所以调用重新获取刷新页面
-              this.chargeDialog = false;
-              this.getdealerList_();
-            }).catch(e => {
-              this.$message(e)
-              this.loading = false
-            })
-          }).catch(() => {
-          });
-        }
-      },
-      // 删除用户
-      deleteuser(row){
-        this.$confirm('确定要删除当前数据吗, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          let par = {
-            id:row.id,
-            state:3
-          }
-          https_f.updateSuserObj(par).then(data => {
-            this.loading = false
-            this.$message({
-              type: 'success',
-              message: '删除成功'
-            });
-            this.getdealerList_();
-          }).catch(e => {
-            this.$message(e)
-            this.loading = false
-          })
-        }).catch(() => {
-        });
-      },
-      //关闭或开启
-      closeorstrat(row){
-        this.$confirm('确定要改变当前数据状态吗, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          let param ={
-            id:row.id
-          }
-          if(row.state == 1){
-            param .state = 0;
-          }else{
-            param .state = 1;
-          }
-          https_f.updateSuserObj(param).then(data => {
-            this.$message({
-              type: 'success',
-              message: '执行成功!'
-            });
-            this.getdealerList_();
-            //状态如果改变成功 直接通过传递的 对象改变当前表格绑定的数据值
-            if(row.state == 1){
-              row.state = 0;
-            }else{
-              row.state = 1;
-            }
-          }).catch(e => {
-            console.log(JSON.stringify(e));
-            this.$message("执行失败!");
-          })
-        }).catch(() => {
-          console.log("取消");
-        });
       },
       param_handle(arr){
         let newarr = arr;
@@ -290,27 +143,38 @@
         if(newarr.shopName == ''){
           delete newarr['shopName'];
         }
+        delete newarr['auditState'];
         delete newarr['wxNickName'];
         delete newarr['name'];
         delete newarr['wxAppId'];
         return newarr;
-      },
-      /*用户授权 AAAAAAAAAAAAAA*/
-      userAuthRole(id){
-        console.log('');
-        this.$router.push({name:'suser_roleAuthorizationcp', query:{id}})
       },
       /*获取功能数据列表*/
       getdealerList_() {
         this.loading = true;
         https_f.dealer_List(this.param_handle(this.dealerInfo)).then(data => {
           this.loading = false
-          console.log("传递参数："+JSON.stringify(this.param_handle(this.dealerInfo)));
-          console.log("返回数据："+JSON.stringify(data));
-
           this.dealerList = data.dataList;
           for(let x=0;x<this.dealerList.length;x++){
-            this.dealerList[x].createTime =  this.formatDate_(this.dealerList[x].createTime);
+
+             console.log(this.dealerList[x].createDate);
+
+            if(this.dealerList[x].type == 1){
+              this.dealerList[x].dealertypename = "店老板";
+            }else{
+              this.dealerList[x].dealertypename = "员工";
+            }
+            if(this.dealerList[x].type == 1){
+              this.dealerList[x].dealerperm = "店老板";
+            }else{
+              if(this.dealerList[x].permissionState == 1){
+                this.dealerList[x].dealerperm = "管理员";
+              }else{
+                this.dealerList[x].dealerperm = "普工";
+              }
+            }
+            // dealerperm
+            this.dealerList[x].createDate =  this.formatDate_(this.dealerList[x].createDate);
           }
           this.totalCount = data.pager.recordCount;
         }).catch(e => {
@@ -330,29 +194,10 @@
           auditState:6,
         }
         this.null =0;
-        this.applyInfo={
-          // shopName:'',
-          phone:'',
-          address:'',
-          name:'',
-          createDate:'',
-          logoIamgeUrls:[],
-          userId:'',
-        }
       },
       getSelectDate(value) {
         this.dealerInfo.startTime = value[0]
         this.dealerInfo.endTime = value[1]
-      },
-      //  修改
-      updateuser(row) {
-        // let id = row.id;
-        this.$router.push({name:`suserUpdate`, query:{row}})
-      },
-      //详情
-      goToDetail(row){
-        let id = row.id;
-        this.$router.push({name:`dealerInfo`, query:{id}})
       },
       toggleState(row, index) {
       },
@@ -400,15 +245,6 @@
       this.dealerInfo.type=6;
       this.dealerInfo.name='';
       this.dealerInfo.phone='';
-      this.applyInfo={
-        // shopName:'',
-        phone:'',
-        address:'',
-        name:'',
-        createDate:'',
-        logoIamgeUrls:[],
-        userId:'',
-      }
       this.getdealerList_();
     }
   }
